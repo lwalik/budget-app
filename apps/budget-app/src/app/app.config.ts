5b42c6123a01f65b6d08ed5465ea3533c46aa9a4
lwalik <l.walicki1@gmail.com>
2024-04-17 16:22:34 +0200

    feat: firestore init

--- apps/budget-app/src/app/app.config.ts
+++ apps/budget-app/src/app/app.config.ts
@@ -1,7 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom([
+      AngularFirestoreModule,
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+    ]),
+  ],
 };
---
+++ apps/budget-app/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCSiKoR8A-03Ch4E2fNTjgGCLvLTQS1_PE',
+    authDomain: 'budget-app-wit.firebaseapp.com',
+    projectId: 'budget-app-wit',
+    storageBucket: 'budget-app-wit.appspot.com',
+    messagingSenderId: '1063541746333',
+    appId: '1:1063541746333:web:4a30c737d911e5259d4274',
+  },
+};
