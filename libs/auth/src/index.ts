b84cfda6ee207de800a16133f2bc3d1aced3253c
walik <l.walicki1@gmail.com>
2024-04-25 00:23:17 +0200

    feat: basic loginComponent created

---
+++ libs/auth/src/index.ts
@@ -0,0 +1,2 @@
+// Components
+export * from './lib/components/login/login.component';
---
+++ libs/auth/src/lib/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { take } from 'rxjs';
+
+@Component({
+  selector: 'lib-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {
+  readonly loginForm: FormGroup = new FormGroup({
+    email: new FormControl<string>('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: new FormControl<string>('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+
+  constructor(private readonly _authService: AuthService) {}
+
+  onSignInBtnClicked(form: FormGroup): void {
+    this._authService
+      .login({
+        email: form.get('email')?.value,
+        password: form.get('password')?.value,
+      })
+      .pipe(take(1))
+      .subscribe();
+  }
+}
