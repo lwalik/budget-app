5eaef65bb5e3e40aa82d350aaf8db93c0884774c
lwalik <l.walicki1@gmail.com>
2024-05-14 09:56:17 +0200

    feat: SimpleSelectInputComponent created

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,5 +1,6 @@
 // Components
 export * from './lib/components/simple-input-form/simple-input-form.component';
+export * from './lib/components/simple-select-input/simple-select-input.component';
 export * from './lib/components/simple-modal/simple-modal.component';
 // Utils
 export * from './lib/utils/rxjs';
