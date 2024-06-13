80084f88874509353c4400747f902dbb300f850c
walik <l.walicki1@gmail.com>
2024-06-14 01:40:53 +0200

    feat: resolvers fixed

--- libs/products/src/lib/resolvers/load-products.resolver.ts
+++ libs/products/src/lib/resolvers/load-products.resolver.ts
@@ -1,15 +1,10 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
-import { Observable, of, switchMap, take } from 'rxjs';
+import { Observable, of, switchMap, take, tap } from 'rxjs';
 import { ProductsState } from '../states/products.state';
 
 export const LoadProductsResolver: ResolveFn<void> = (): Observable<void> => {
   const productsState: ProductsState = inject(ProductsState);
 
-  return productsState.isInitialized$.pipe(
-    take(1),
-    switchMap((isInitialized: boolean) =>
-      isInitialized ? of(void 0) : productsState.loadProducts()
-    )
-  );
+  return productsState.loadProducts();
 };
--- libs/wallets/src/lib/resolvers/load-wallets.resolver.ts
+++ libs/wallets/src/lib/resolvers/load-wallets.resolver.ts
@@ -1,15 +1,10 @@
 import { ResolveFn } from '@angular/router';
-import { Observable, of, switchMap, take } from 'rxjs';
+import { Observable, of, switchMap, take, tap } from 'rxjs';
 import { WalletsState } from '../states/wallets.state';
 import { inject } from '@angular/core';
 
 export const LoadWalletsResolver: ResolveFn<void> = (): Observable<void> => {
   const walletsState: WalletsState = inject(WalletsState);
 
-  return walletsState.isInitialized$.pipe(
-    take(1),
-    switchMap((isInitialized: boolean) =>
-      isInitialized ? of(void 0) : walletsState.loadWallets()
-    )
-  );
+  return walletsState.loadWallets();
 };
