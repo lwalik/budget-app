fa9b81c045c14a2db44894e0b26c4b548e6a045e
walik <l.walicki1@gmail.com>
2024-06-21 14:59:04 +0200

    feat: translations created

---
+++ libs/i18n/src/lib/translations/translations.ts
@@ -0,0 +1,24 @@
+import { TranslationType } from '../types/translation.type';
+
+export const translations: TranslationType = {
+  Dashboard: {
+    pl: 'Pulpit',
+    en: 'Dashboard',
+  },
+  Wallets: {
+    pl: 'Portfele',
+    en: 'Wallets',
+  },
+  Products: {
+    pl: 'Produkty',
+    en: 'Products',
+  },
+  Expenses: {
+    pl: 'Wydatki',
+    en: 'Expenses',
+  },
+  Summary: {
+    pl: 'Podsumowanie',
+    en: 'Summary',
+  },
+};
